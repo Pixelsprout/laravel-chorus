49e0e75c7333e71ed7c613f2d7957d4b07b59e1c
Braeden Foster <braedenleefoster@gmail.com>
2025-07-30 15:21:14 +1200

    fix: issue with composer packages.

--- packages/chorus/tests/Feature/ExampleTest.php
+++ packages/chorus/tests/Feature/ExampleTest.php
@@ -1,7 +1,7 @@
 <?php
 
-it('returns a successful response', function () {
-    $response = $this->get('/');
+it('returns schema endpoint response', function () {
+    $response = $this->get('/api/schema');
 
     $response->assertStatus(200);
 });
