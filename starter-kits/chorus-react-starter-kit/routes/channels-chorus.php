b705dd3345741a26b8fc2bc251ffeb8080addc5f
Braeden Foster <braedenleefoster@gmail.com>
2025-07-30 18:02:59 +1200

    chore: installed Chorus

---
+++ starter-kits/chorus-react-starter-kit/routes/channels-chorus.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Broadcast;
+
+// Chorus channels
+Broadcast::channel('chorus.user.{id}', function ($user, $id) {
+    return $user->id === $id;
+});
+
+// Prefixed Chorus channels (e.g., chorus.tenantId.user.123)
+//Broadcast::channel("chorus.{prefix}.user.{id}", function ($user, $prefix, $id) {
+//    // Ensure the user is authenticated and their ID matches the channel ID
+//    // Also, validate that the user's tenant_id matches the prefix
+ //   return (int) $user->id === (int) $id && (string) $user->tenant_id === (string) $prefix;
+//});
