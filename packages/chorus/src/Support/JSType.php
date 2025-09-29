967480c5d60b430f211ff03ca2fc61fbd40a5a56
Braeden Foster <braeden.foster@fostermoore.com>
2025-09-29 15:02:46 +1300

    Add JS type utilities and normalise type definitions

--- packages/chorus/src/Support/JSType.php
+++ packages/chorus/src/Support/JSType.php
@@ -6,10 +6,32 @@ namespace Pixelsprout\LaravelChorus\Support;
 
 enum JSType: string
 {
-    case String = 'string';
-    case Number = 'number';
-    case Boolean = 'boolean';
-    case Object = 'object';
-    case Array = 'array';
-    case Any = 'any';
+    case String = "string";
+    case Int = "int";
+    case Number = "number";
+    case Boolean = "boolean";
+    case Array = "array";
+    case Object = "object";
+    case Null = "null";
+    case Undefined = "undefined";
+
+    public function optional(): string
+    {
+        return "$this->value?";
+    }
+
+    public static function union(JSType ...$types): string
+    {
+        return implode(" | ", array_map(fn($t) => $t->value, $types));
+    }
+
+    public static function arrayOf(JSType $type): string
+    {
+        return "$type->value[]";
+    }
+
+    public static function objectOf(JSType $valueType): string
+    {
+        return "Record<string, {$valueType->value}>";
+    }
 }
